41f18493b21219ca9a274d0e98aa0219fd9ccffd
hongliang.zang <22933133+HongliangZang@users.noreply.github.com>
2021-04-04 15:58:37 +0100

    override get letter grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,10 +1,45 @@
-﻿namespace GradeBook.GradeBooks
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
+        public int NoToDropAGrade {
+            get
+            {
+                return Students.Count / 5;
+            }
+        }
+
+        public List<Student> SortedStudents {
+            get
+            { return Students.OrderBy(x => x.AverageGrade).ToList(); }
+        }
+
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            if (averageGrade >= SortedStudents[NoToDropAGrade].AverageGrade)
+            { return 'A'; }
+            else if (averageGrade >= SortedStudents[2 * NoToDropAGrade].AverageGrade)
+            { return 'B'; }
+            else if (averageGrade >= SortedStudents[3 * NoToDropAGrade].AverageGrade)
+            { return 'C'; }
+            else if (averageGrade >= SortedStudents[4 * NoToDropAGrade].AverageGrade)
+            { return 'D'; }
+            else            
+                return 'F';
         }
     }
 }
